fea3202a7694bacf21ee3468cdd12208d8d269f5
satyamsen7 <164318200+satyamsen7@users.noreply.github.com>
2025-01-03 22:36:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello, MERN stack!');
+});
+
+// Connect to MongoDB
+const URI = 'your_mongoDB_connection_string';
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
